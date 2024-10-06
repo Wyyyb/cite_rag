2d84e59d2470762d5f32c845bd0ad71d7bd972e9
Wyyyb <yubo.wang.sunny@gmail.com>
2024-10-07 04:46:33 +0800

    add

--- arxiv_data_collect/collect_latex_data.py
+++ arxiv_data_collect/collect_latex_data.py
@@ -59,7 +59,7 @@ def download():
     exist_ids = []
     for file in os.listdir(target_folder):
         if os.path.isdir(os.path.join(target_folder, file)):
-            print("exist file", file)
+            # print("exist file", file)
             exist_ids.append(file)
     for file in os.listdir(input_dir):
         if not file.endswith(".json"):
@@ -69,8 +69,8 @@ def download():
             for k, v in curr.items():
                 if k not in exist_ids:
                     eprint_id_list.append(k)
-                else:
-                    print("skip it")
+                # else:
+                #     print("skip it")
     multi_process_download(eprint_id_list, target_folder)
 
 
